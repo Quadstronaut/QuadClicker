50882338f657145a940a8a17098eb10d56dcac10
gitkagehub <khgreenprime@gmail.com>
2026-01-09 00:27:36 -0700

    Added 'App.xaml.cs' with basic application initialization

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace QuadClicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
